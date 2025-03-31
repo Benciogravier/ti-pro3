0948fb7931a269b34f215584fa1d1fb29a77bb56
Lucas-Caronni <lcaronni@udesa.edu.ar>
2025-03-31 11:06:16 -0300

    Componente footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css'
+
+function Footer (){
+    return(
+        <footer>
+        <p>Lucas Caronni - Benicio Gravier - Joaquin Rosas Zuiani</p>
+    </footer>
+    )
+}
+
+export default Footer;
