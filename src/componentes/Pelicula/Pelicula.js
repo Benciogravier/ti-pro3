a51a29adcc11a8e51ac643d42fb72a897056ff90
Beniciogravier <bgravier@udesa.edu.ar>
2025-04-06 23:09:53 -0300

    le dimos estado y eventos al componente pelicula

--- src/componentes/Pelicula/Pelicula.js
+++ src/componentes/Pelicula/Pelicula.js
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import {Link} from 'react-router-dom';
 
-function Pelicula(props) {
-    return(
-        <div className="pelicula-cart-card">
-            <img className="img-cart-peli" src={`./img/${props.data.img}`} alt={props.data.name}/>
-            <h3>{props.data.name}</h3>
-            <button>Ver descripcion</button>
-            <h4>Decripcion: tenemos que hacerla desaparecer</h4>
-            <Link to={`/detalle/${props.data.id}`}>
-                <button>Detalle</button>   
-            </Link>
-            <button>Agregar a favoritos / quitar</button>
-        </div>
-    )
+class Pelicula extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data: props.data,
+            mostrarContenido: false,
+            favorito: false
+        }
+    }
+
+    ocultar(){
+        this.setState({
+            mostrarContenido: !this.state.mostrarContenido
+        })
+    }
+
+    agregarFav(){
+        this.setState({
+            favorito: !this.state.favorito
+        })
+    }
+
+    render() {
+        return(
+            <div className="pelicula-cart-card">
+                <img className="img-cart-peli" src={`./img/${this.state.data.img}`} alt={this.state.data.name}/>
+                <h3>{this.state.data.name}</h3>
+                {
+                    this.state.mostrarContenido === true ? (
+                    <>
+                        <h4>Descripcion:</h4>
+                        <p> {this.state.data.description} </p>
+                        <button onClick={() => this.ocultar()}>Ocultar Descripcion</button>
+                    </>
+                    ) : null
+                }
+                {
+                    this.state.mostrarContenido === false ? (
+                        <button onClick={() => this.ocultar()}>Ver descripcion</button>
+                    ) : null                    
+                }
+
+                <Link to={`/detalle/${this.state.data.id}`}>
+                    <button>Detalle</button>   
+                </Link>
+
+                {
+                    this.state.favorito === false ? (
+                        <button onClick={() => this.agregarFav()}>Agregar a favoritos</button>
+                    ) : null
+                }
+                {
+                    this.state.favorito === true ? (
+                        <button onClick={() => this.agregarFav()}>Quitar de favoritos</button>
+                    ) : null
+                }
+
+
+            </div>
+        )
+    }
 }
+
 
 export default Pelicula;
