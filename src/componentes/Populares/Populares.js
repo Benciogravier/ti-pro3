2d8c9b1d646434acd0ba1e7f50b528642892df60
JoacoRosas <jrosaszuiani@udesa.edu.ar>
2025-04-13 19:04:56 -0300

    arregle url de populares y la url de img en pelicula

--- src/componentes/Populares/Populares.js
+++ src/componentes/Populares/Populares.js
@@ -18,7 +18,7 @@ class Populares extends Component{
               Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4N2RlMGQwZjJjMmEyNDBkZjc0NmQxYmQ3MGMxYzg0NiIsIm5iZiI6MTc0MzUzMTIyNC4yMDk5OTk4LCJzdWIiOiI2N2VjMmNkODE5ZjFiMWNiNGVmYTBiNjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mzcXwSEblhmvnWTV6blXwTJFlggcgIwLklQqYTZPseA'
             }
           };
-        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
             .then((results) => results.json())
             .then((data) => this.setState({
                 peliculas: data.results.slice(15),
