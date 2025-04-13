5f9f5bc618e7bf16847e07071d26743bd2de8ed3
Lucas-Caronni <lcaronni@udesa.edu.ar>
2025-04-13 19:37:35 -0300

    Detalle componente y screen

--- src/screens/Detalle/Detalle.js
+++ src/screens/Detalle/Detalle.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
+import DetallePelicula from "../../componentes/DetallePelicula/DetallePelicula";
 
-function Detalle(props){
-    const idPelicula = props.match.params.id
-    return(
-        <div>
-            <h1>Detalle de la película {idPelicula}</h1>
-        </div>
-    )
+class Detalle extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        pelicula: null
+      };
+    }
+  
+    componentDidMount() {
+      const id = this.props.match.params.id;
+      const url = `https://api.themoviedb.org/3/movie/${id}?language=es-ES&api_key=Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4N2RlMGQwZjJjMmEyNDBkZjc0NmQxYmQ3MGMxYzg0NiIsIm5iZiI6MTc0MzUzMTIyNC4yMDk5OTk4LCJzdWIiOiI2N2VjMmNkODE5ZjFiMWNiNGVmYTBiNjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mzcXwSEblhmvnWTV6blXwTJFlggcgIwLklQqYTZPseA`;
+
+      const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+        }
+      };
+  
+      fetch(url, options)
+        .then((results) => results.json())
+        .then((data) => this.setState({ 
+            pelicula: data 
+        }))
+        .catch(error => console.log('Error al obtener los datos:', error));
+    }
+
+    render(){
+        return(
+            <section>
+                <h1>Detalle de la Pelicula</h1>
+                {this.state.pelicula === null ? (
+                    <h2>Cargando los datos...</h2>
+                ):(
+                    <DetallePelicula dara={this.state.pelicula} />
+                )}
+            </section>
+        )
+    }
+    
 }
 
-export default Detalle;
+export default Detalle
